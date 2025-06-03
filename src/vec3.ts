502ee96795ed3db4e992764fbaeffdd8c8f47b34
Oscar <oscarrpeulicke@gmail.com>
2025-06-03 17:03:35 +0200

    more vec3 functions

--- src/vec3.ts
+++ src/vec3.ts
@@ -2,6 +2,8 @@ export type Vec3 = [number, number, number];
 
 export const clone = (v: Vec3): Vec3 => [...v];
 
+export const isVec3 = (v: unknown): v is Vec3 => Array.isArray(v) && v.length === 3;
+
 export const negate = (v: Vec3): Vec3 => [-v[0], -v[1], -v[2]];
 
 export const add = (a: Vec3, b: Vec3): Vec3 => [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
@@ -30,6 +32,10 @@ export const proj = (a: Vec3, b: Vec3): Vec3 => scale(b, dot(a, b) / dot(b, b));
 
 export const projPlane = (a: Vec3, b: Vec3): Vec3 => sub(a, proj(a, b));
 
+export const dist = (a: Vec3, b: Vec3): number => length(sub(b, a));
+
+export const dir = (a: Vec3, b: Vec3, l: number): Vec3 => scale(normalize(sub(b, a)), l);
+
 export const min = (a: Vec3, b: Vec3): Vec3 => [Math.min(a[0], b[0]), Math.min(a[1], b[1]), Math.min(a[2], b[2])];
 
 export const max = (a: Vec3, b: Vec3): Vec3 => [Math.max(a[0], b[0]), Math.max(a[1], b[1]), Math.max(a[2], b[2])];
@@ -41,6 +47,22 @@ export const floor = (v: Vec3): Vec3 => [Math.floor(v[0]), Math.floor(v[1]), Mat
 export const ceil = (v: Vec3): Vec3 => [Math.ceil(v[0]), Math.ceil(v[1]), Math.ceil(v[2])];
 
 export const abs = (v: Vec3): Vec3 => [Math.abs(v[0]), Math.abs(v[1]), Math.abs(v[2])];
+
+export const getNearestObject = <T>(obj: T | Vec3, allObjs: T[], pos: (t: T) => Vec3): T | undefined => {
+    const objPos = isVec3(obj) ? obj : pos(obj);
+    let result: T | undefined = undefined;
+    let minDist = Infinity;
+    for (const o of allObjs) {
+        if (o === obj) continue;
+        const d = dist(objPos, pos(o));
+        if (d > minDist) continue;
+        minDist = d;
+        result = o;
+    }
+    return result;
+};
+
+export const getNearest = (v: Vec3, all: Vec3[]) => getNearestObject(v, all, t => t);
 
 export const normalize = (v: Vec3): Vec3 => {
     const l = length(v);

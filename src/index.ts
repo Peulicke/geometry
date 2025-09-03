dc254130b01cba1bb985e211d22eeb286f9e33b9
Oscar <oscarrpeulicke@gmail.com>
2025-09-03 19:32:09 +0200

    transformation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * as grid from "./grid.js";
-export * as vec2 from "./vec2.js";
-export * as vec3 from "./vec3.js";
 export * as orient from "./orient.js";
 export * as plane from "./plane.js";
+export * as transformation from "./transformation.js";
+export * as vec2 from "./vec2.js";
+export * as vec3 from "./vec3.js";

4511f7b6d66653523f8fc87e11bb4ba4996b5c89
Oscar <oscarrpeulicke@gmail.com>
2025-12-23 16:22:23 +0100

    lineTriangleIntersection

--- src/triangle.ts
+++ src/triangle.ts
@@ -1,5 +1,6 @@
+import { mapTuple } from "@peulicke/algorithms/tuple";
 import { getLineDir, type Line } from "./line.js";
-import type { Plane } from "./plane.js";
+import { isBehindPlane, linePlaneIntersection, type Plane } from "./plane.js";
 import * as vec3 from "./vec3.js";
 
 export type Triangle = [vec3.Vec3, vec3.Vec3, vec3.Vec3];
@@ -11,4 +12,23 @@ export const triangleToPlane = (t: Triangle): Plane => {
         pos: t[0],
         dir: vec3.cross(getLineDir(line1), getLineDir(line2))
     };
+};
+
+export const triangleToLines = (t: Triangle): [Line, Line, Line] =>
+    mapTuple(t, (point, i) => ({ from: point, to: t[(i + 1) % 3]! }));
+
+export const triangleToOutwardPlanes = (t: Triangle): [Plane, Plane, Plane] => {
+    const plane = triangleToPlane(t);
+    const lines = triangleToLines(t);
+    return mapTuple(lines, (line, i) => ({
+        pos: t[i]!,
+        dir: vec3.cross(getLineDir(line), plane.dir)
+    }));
+};
+
+export const lineTriangleIntersection = (line: Line, triangle: Triangle): vec3.Vec3 | undefined => {
+    const point = linePlaneIntersection(line, triangleToPlane(triangle));
+    const planes = triangleToOutwardPlanes(triangle);
+    if (!planes.every(plane => isBehindPlane(point, plane))) return undefined;
+    return point;
 };
